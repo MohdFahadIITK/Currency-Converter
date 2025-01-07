a5b0247d7d1c6f5ac5585302f73eb8b87d67b6c2
MohdFahadIITK <fahadm20@iitk.ac.in>
2025-01-07 17:28:31 +0530

    hiii

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ConvertedPage from './ConvertedPage';
 import NewPage from './NewPage';
 function App() {
   return (
-    <Router>
+    <Router >
       <Routes>
         <Route path="/" element={<NewPage />} />
         <Route path="/ConvertedPage" element={<ConvertedPage />} />
